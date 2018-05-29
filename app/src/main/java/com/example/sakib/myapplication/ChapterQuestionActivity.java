00899d8b279bf912adaa9abbd652735aedc2baa9
1405043-kd <1405043-kd@ugrad.cse.buet.ac.bd>
2018-05-29 20:03:26 +0600

    #local_host_question_fect_working

--- app/src/main/java/com/example/sakib/myapplication/ChapterQuestionActivity.java
+++ app/src/main/java/com/example/sakib/myapplication/ChapterQuestionActivity.java
@@ -2,11 +2,26 @@ package com.example.sakib.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import com.example.sakib.myapplication.models.Adapters.QuestionAdapter;
+import com.example.sakib.myapplication.models.QuestionClient;
+import com.example.sakib.myapplication.models.Questions;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ChapterQuestionActivity extends AppCompatActivity {
     String apiStr="";
-
+    ListView questionList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +33,44 @@ public class ChapterQuestionActivity extends AppCompatActivity {
         }
 
         Toast.makeText(ChapterQuestionActivity.this,apiStr, Toast.LENGTH_SHORT).show();
+
+        questionList = (ListView) findViewById(R.id.question_pagination_list);
+
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("http://192.168.0.104:8000/")
+                //    .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.build();
+        QuestionClient questionClient = retrofit.create(QuestionClient.class);
+
+        Call<List<Questions>> call_cq =questionClient.cqAll();
+
+
+        call_cq.enqueue(new Callback<List<Questions>>() {
+            @Override
+            public void onResponse(Call<List<Questions>> call, Response<List<Questions>> response) {
+                System.out.println("hcud");
+                List<Questions> questions = response.body();
+
+                questionList.setAdapter(new QuestionAdapter(ChapterQuestionActivity.this, questions));
+                questionList.setOnItemClickListener(new ListView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Questions questions=(Questions) adapterView.getItemAtPosition(i);
+
+                        Toast.makeText(ChapterQuestionActivity.this, questions.getQuestion(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Questions>> call, Throwable t) {
+                Toast.makeText(ChapterQuestionActivity.this, "error :(", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/sakib/myapplication/models/QuestionClient.java
+++ app/src/main/java/com/example/sakib/myapplication/models/QuestionClient.java
@@ -1,4 +1,38 @@
 package com.example.sakib.myapplication.models;
 
-public class QuestionClient {
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface QuestionClient {
+    @GET("/c_q/")
+    Call<List<Questions>> cqAll();
+
+    @GET("/c_q/{sub}/")
+    Call<List<Questions>> cqSub(@Path("sub") String sub);
+
+    @GET("/c_q/{sub}/{chapter}/")
+    Call<List<Questions>> cqSubChap(@Path("sub") String sub, @Path("chapter") String chapter);
+
+    @GET("/x_q/")
+    Call<List<Questions>> xqAll();
+
+    @GET("/x_q/{year}/")
+    Call<List<Questions>> xqYear(@Path("year") String year);
+
+    @GET("/x_q/{year}/{MVD}/")
+    Call<List<Questions>> xqYearMVD(@Path("year") String year, @Path("MVD") String MVD);
+
+    @GET("/q_set/")
+    Call<List<QuestionSet>> q_set();
+
+    @GET("/e_history")
+    Call<List<ExamHistory>> e_history();
+
+    @GET("/e_history/{user_id}/")
+    Call<List<ExamHistory>> e_history_user(@Path("user_id") String user_id);
+
 }
